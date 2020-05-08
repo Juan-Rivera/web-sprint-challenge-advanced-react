7b88da80eb9ca4c2e888bbe79c9de20a6fe1dad8
Juan (programming) <programjuan@Juans-MacBook-Pro.local>
2020-05-08 13:14:30 -0400

    added FireEvent functions to fill in the Form Inputs

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
@@ -15,10 +15,34 @@ test("form header renders", () => {
 test("form shows success message on submit with form details", () => {
     const { getByLabelText } = render(<CheckoutForm />);
 
+    //Form Input Queries
     const firstInput = getByLabelText(/First Name/i);
     const lastInput = getByLabelText(/Last Name/i);
     const addressInput = getByLabelText(/Address/i);
     const cityInput = getByLabelText(/City/i);
     const stateInput = getByLabelText(/State/i);
     const zipInput = getByLabelText(/Zip/i)
+
+    //FireEvent functions
+    fireEvent.change(firstInput, {
+        target: { name: "firstName", value: "John" }
+      });
+    fireEvent.change(lastInput, {
+        target: { name: "lastName", value: "Doe" }
+    });
+    fireEvent.change(addressInput, {
+        target: { name: "address", value: "123 Street Rd" }
+    });
+    fireEvent.change(cityInput, {
+        target: { name: "city", value: "Boston" }
+    });
+    fireEvent.change(stateInput, {
+        target: { name: "state", value: "Massachusetts" }
+    });
+    fireEvent.change(zipInput, {
+        target: { name: "zip", value: "02114" }
+    }); 
+
+    console.log(firstInput.value)
+
 });
